183aee5b7d13809d68673e9f844c37aee35f0660
moses-adeyemi <oludareadeyemi52@gmail.com>
2022-11-29 09:51:34 +0100

    firstcommits2

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+// import "./App.css";
+
+
+
+function App() {
+
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+
+
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+    </main>
+  );
+
+}
+
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import SignupForm from './SignupForm';
+
+
+
+const Form = () => {
+  return (
+    <div>
+        <SignupForm/>
+    </div>
+  )
+}
+
+export default Form;
